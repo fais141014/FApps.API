552967beca14cca4e25af29ad400e530fa6f7cfb
fais141014 <fais141014@gmail.com>
2019-11-11 18:03:58 +0530

    added Upadte and delete contacts

---
+++ FApps.API/FApps.Services/Contact/CommandHandlers/DeleteCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using FApps.Core.Extension;
+using FApps.Data.Interfaces;
+using FApps.Services.Contact.Command;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FApps.Services.Contact.CommandHandlers
+{
+    public class DeleteCommandHandler:IRequestHandler<DeleteCommand, bool>
+    {
+        private readonly IContactService _service;
+        public DeleteCommandHandler(IContactService service)
+        {
+            _service = service;
+        }
+
+        public async Task<bool>Handle(DeleteCommand request, CancellationToken cancellationToken)
+        {
+            request.ThrowIfNull(nameof(request));
+            var contact = await _service.GetContactByIdAsync((Guid)request.Id);
+            if(contact == null) { return false; }
+            else
+            {
+               return await _service.DeleteContactAsync(contact);
+            }
+        }
+    }
+}
---
+++ FApps.API/FApps.Services/Contact/CommandHandlers/UpdateCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using FApps.Data.Interfaces;
+using FApps.Services.Contact.Command;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FApps.Services.Contact.CommandHandlers
+{
+    public class UpdateCommandHandler:IRequestHandler<UpdateCommand,bool>
+    {
+        private readonly IContactService _service;
+        public UpdateCommandHandler(IContactService service)
+        {
+            _service = service;
+        }
+        public async Task<bool> Handle(UpdateCommand request, CancellationToken cancellationToken)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            //request.contactModel.Id = request.id;
+            var contact = await _service.GetContactByIdAsync(request.id);
+            if (contact == null) { return false; }
+            else
+            {
+                return await _service.UpdateContactAsync(request.contactModel);
+            }
+        }
+    }
+}
